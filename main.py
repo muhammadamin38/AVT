2d49366834cb31cb6a6a53e36ca0a48286f6eecc
Your Name <you@example.com>
2022-12-19 14:31:45 +0400

    frist commit

---
+++ main.py
@@ -0,0 +1,13 @@
+amal = input("Amalni kiriting: ")
+a = int(input("1-sonni kiriting: "))
+b = int(input("2-sonni kiriting: "))
+
+if amal == "+":
+    print(a + b)
+elif amal =="-":
+    print(a - b)
+elif amal =="*":
+    print(a * b) 
+elif amal =="/":
+    print(a / b)
+    
